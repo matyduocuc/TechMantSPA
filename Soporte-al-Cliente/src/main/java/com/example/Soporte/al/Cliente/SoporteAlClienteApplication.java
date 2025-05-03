dc7b64f7cf05a4590455fa69e653ba61e93eb3c1
Ignaciodrx908 <ig.londono@duocuc.cl>
2025-05-03 17:38:32 -0400

    Add files via upload

---
+++ Soporte-al-Cliente/src/main/java/com/example/Soporte/al/Cliente/SoporteAlClienteApplication.java
@@ -0,0 +1,13 @@
+package com.example.Soporte.al.Cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoporteAlClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoporteAlClienteApplication.class, args);
+	}
+
+}
