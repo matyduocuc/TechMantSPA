6cdf89acf15cdd80fcd1ed36a5c2a3c6f41e9737
Ignaciodrx908 <ig.londono@duocuc.cl>
2025-05-03 17:43:19 -0400

    Add files via upload

---
+++ Gestión de Sucursales/src/main/java/com/example/Gestion/de/Sucursales/GestionDeSucursalesApplication.java
@@ -0,0 +1,13 @@
+package com.example.Gestion.de.Sucursales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeSucursalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeSucursalesApplication.class, args);
+	}
+
+}
