6df34308d04a8b422a511a6b0c9d734ea7c0bcf3
Ignaciodrx908 <ig.londono@duocuc.cl>
2025-05-03 17:40:30 -0400

    Add files via upload

---
+++  Logística y Envíos/src/main/java/com/example/Logistica/y/Envios/LogisticaYEnviosApplication.java
@@ -0,0 +1,13 @@
+package com.example.Logistica.y.Envios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticaYEnviosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticaYEnviosApplication.class, args);
+	}
+
+}
