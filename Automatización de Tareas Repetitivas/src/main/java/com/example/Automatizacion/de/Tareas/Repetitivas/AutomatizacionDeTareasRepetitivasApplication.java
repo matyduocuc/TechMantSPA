c892f95ded28bbb815225ea7ecaa45ffcb6ba0a9
Ignaciodrx908 <ig.londono@duocuc.cl>
2025-05-03 17:44:18 -0400

    Add files via upload

---
+++ Automatización de Tareas Repetitivas/src/main/java/com/example/Automatizacion/de/Tareas/Repetitivas/AutomatizacionDeTareasRepetitivasApplication.java
@@ -0,0 +1,13 @@
+package com.example.Automatizacion.de.Tareas.Repetitivas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatizacionDeTareasRepetitivasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatizacionDeTareasRepetitivasApplication.class, args);
+	}
+
+}
---
+++ Automatización de Tareas Repetitivas/src/test/java/com/example/Automatizacion/de/Tareas/Repetitivas/AutomatizacionDeTareasRepetitivasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Automatizacion.de.Tareas.Repetitivas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatizacionDeTareasRepetitivasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
