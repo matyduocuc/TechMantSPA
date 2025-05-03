cd9ee125fd0eb1d2b9c79fcb07b2db07ba1c7916
Ignaciodrx908 <ig.londono@duocuc.cl>
2025-05-03 17:21:17 -0400

    Add files via upload

---
+++ Monitoreo-de-Equipos/src/main/java/com/example/Monitoreo/de/Equipos/MonitoreoDeEquiposApplication.java
@@ -0,0 +1,13 @@
+package com.example.Monitoreo.de.Equipos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoreoDeEquiposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoreoDeEquiposApplication.class, args);
+	}
+
+}
