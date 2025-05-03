47808e352a635a4fe6e624043a17f24a9aeb2e46
maty <mati.gaetet@duocuc.cl>
2025-05-03 17:32:17 -0400

    Add files via upload

---
+++ _Inventario de Repuestos/Inventario-de-Repuestos/src/main/java/com/example/Inventario/de/Repuestos/InventarioDeRepuestosApplication.java
@@ -0,0 +1,13 @@
+package com.example.Inventario.de.Repuestos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioDeRepuestosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarioDeRepuestosApplication.class, args);
+	}
+
+}
---
+++ _Inventario de Repuestos/Inventario-de-Repuestos/src/test/java/com/example/Inventario/de/Repuestos/InventarioDeRepuestosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Inventario.de.Repuestos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioDeRepuestosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
