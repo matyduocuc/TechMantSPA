a3be27b00c24ed5e6815becfba64a357298ee5a1
Ignaciodrx908 <ig.londono@duocuc.cl>
2025-05-11 10:23:45 -0400

    Add files via upload

---
+++ Gestion-de-usuarios/src/test/java/com/example/Gestion/de/usuarios/GestionDeUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Gestion.de.usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
