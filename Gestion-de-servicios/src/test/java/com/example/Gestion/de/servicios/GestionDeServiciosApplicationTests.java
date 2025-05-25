6e8707004061c570e0ea3e3325c03fbad594386d
Ignaciodrx908 <ig.londono@duocuc.cl>
2025-05-25 17:00:34 -0400

    Add files via upload

---
+++ Gestion-de-servicios/src/test/java/com/example/Gestion/de/servicios/GestionDeServiciosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Gestion.de.servicios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeServiciosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
