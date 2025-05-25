50ac2c0cae378e78fa2ba892c6313f1bf138da01
Ignaciodrx908 <ig.londono@duocuc.cl>
2025-05-25 16:55:37 -0400

    Add files via upload

---
+++ Gestion de ventas/Gestion-de-ventas/src/main/java/com/example/Gestion/de/ventas/GestionDeVentasApplication.java
@@ -0,0 +1,13 @@
+package com.example.Gestion.de.ventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeVentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeVentasApplication.class, args);
+	}
+
+}
---
+++ Gestion de ventas/Gestion-de-ventas/src/test/java/com/example/Gestion/de/ventas/GestionDeVentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Gestion.de.ventas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeVentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
