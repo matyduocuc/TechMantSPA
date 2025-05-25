d2728b9f6fde3a5f9c9c37776708de8abb0a60f4
Ignaciodrx908 <ig.londono@duocuc.cl>
2025-05-25 15:34:59 -0400

    Add files via upload

---
+++ Servicio/Servicio/src/test/java/com/example/Servicio/ServicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Servicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
