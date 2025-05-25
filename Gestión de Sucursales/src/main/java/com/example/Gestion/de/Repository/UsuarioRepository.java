9b2cfb7c10e7ae332b424235a84a43d295932319
Ignaciodrx908 <ig.londono@duocuc.cl>
2025-05-25 14:52:30 -0400

    Delete Gestión de Sucursales directory

--- Gestión de Sucursales/src/main/java/com/example/Gestion/de/Repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Gestion.de.Repository;
-
-import com.example.Gestion.de.dto.UsuarioDTO;
-import com.example.gestiondesucursales.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsuarioRepository extends JpaRepository<UsuarioDTO, Long> {}
--- Gestión de Sucursales/src/main/java/com/example/Gestion/de/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.Gestion.de.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
