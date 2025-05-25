c71169a8b9c6277872ab8ba62a4f89ce398e789c
Ignaciodrx908 <ig.londono@duocuc.cl>
2025-05-25 15:33:17 -0400

    Delete Gestión de Garantías directory

--- Gestión de Garantías/Gestión de Garantías/src/main/java/com/example/Gestion/de/Garantias/repository/GarantiaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Gestion.de.Garantias.repository;
-
-import com.example.Gestion.de.Garantias.model.Garantia;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GarantiaRepository extends JpaRepository<Garantia, Long> {
-}
