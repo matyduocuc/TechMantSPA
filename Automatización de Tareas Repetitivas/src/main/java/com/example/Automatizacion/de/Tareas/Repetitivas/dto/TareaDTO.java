126f93ef0db3f26c5b46839297cf5a43b9e87d62
maty <mati.gaetet@duocuc.cl>
2025-05-27 16:11:00 -0400

    Delete Automatización de Tareas Repetitivas directory

--- Automatización de Tareas Repetitivas/src/main/java/com/example/Automatizacion/de/Tareas/Repetitivas/dto/TareaDTO.java
+++
@@ -1,9 +0,0 @@
-package com.example.Automatizacion.de.Tareas.Repetitivas.dto;
-
-import lombok.Data;
-
-@Data
-public class TareaDTO {
-    private String nombre;
-    private String descripcion;
-}
