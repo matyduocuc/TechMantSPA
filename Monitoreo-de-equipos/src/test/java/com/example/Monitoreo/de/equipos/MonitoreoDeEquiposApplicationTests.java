49fdf13cd1c3338d8e26f5bcf04f39c0adb80c00
Ignaciodrx908 <ig.londono@duocuc.cl>
2025-05-27 11:34:04 -0400

    Add files via upload

---
+++ Monitoreo-de-equipos/src/test/java/com/example/Monitoreo/de/equipos/MonitoreoDeEquiposApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Monitoreo.de.equipos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoreoDeEquiposApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
