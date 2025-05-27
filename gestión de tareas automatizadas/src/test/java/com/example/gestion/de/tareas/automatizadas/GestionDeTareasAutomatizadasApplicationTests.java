598dcfb18d9e4fbf07f9c2f80374440fcef5f047
Ignaciodrx908 <ig.londono@duocuc.cl>
2025-05-27 16:29:49 -0400

    Add files via upload

---
+++ gestión de tareas automatizadas/src/test/java/com/example/gestion/de/tareas/automatizadas/GestionDeTareasAutomatizadasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestion.de.tareas.automatizadas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeTareasAutomatizadasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
